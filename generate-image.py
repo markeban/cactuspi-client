62afa1f5ef18829dc141618e150939ff4bf16fbf
Your Name <you@example.com>
2018-04-17 18:46:26 -0500

    updated python file

--- generate-image.py
+++ generate-image.py
@@ -1,15 +1,18 @@
 import sys
 from PIL import Image, ImageFont, ImageDraw
 
-def colorRed():
-    return (255, 0, 0)
+def colorRandom():
+    rgbl=[255, 0, 0]
+    random.shuffle(rgbl)
+    return tuple(rgbl)
 
 def writeImage(inputString, filename, ledRows):
-    font = ImageFont.truetype("/usr/share/fonts/truetype/wqy/wqy-zenhei.ttc", 28)
+    print 'this is python', inputString, filename
+    font = ImageFont.truetype("/usr/share/fonts/truetype/freefont/FreeSans.ttf", 28)
     width, ignore = font.getsize(inputString)
     im = Image.new("RGB", (width + 30, ledRows), "black")
     draw = ImageDraw.Draw(im)
-    draw.text((0, 0), inputString, colorRed(), font=font)
+    draw.text((0, 0), inputString, colorRandom(), font=font)
 
     im.save(filename)
 
